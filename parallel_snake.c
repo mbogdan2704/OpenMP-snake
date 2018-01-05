6985e8d7815902fd1cdc0cb000306c946890cb13
mbogdan2704 <costin_bogdan.maria@stud.acs.upb.ro>
2018-01-05 05:21:03 -0800

    1

---
+++ parallel_snake.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+void run_simulation(int num_lines, int num_cols, int **world, int num_snakes,
+	struct snake *snakes, int step_count, char *file_name) 
+{
+	
+
+	// TODO: Implement Parallel Snake simulation using the default (env. OMP_NUM_THREADS) 
+	// number of threads.
+	//
+	// DO NOT include any I/O stuff here, but make sure that world and snakes
+	// parameters are updated as required for the final state.
+	
+
+
+
+
+
+}
